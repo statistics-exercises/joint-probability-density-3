22f778f28fc5c1407c1473efe46a249a085bf4e5
Gareth Tribello <gareth.tribello@gmail.com>
2020-11-03 19:06:18 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import sympy as sy
+
+# I have defined the symbols for you to use in your expressions
+x, y, k = sy.symbols('x y k')
+# You need to add a sympy command after the comma here to calculate P(X=x) here
+px = 
+print("P(X=x)=", px )  
+# You need to add a sympy command after the comma here to calculate P(Y=y) here
+py = 
+print("P(Y=y)=", py ) 
